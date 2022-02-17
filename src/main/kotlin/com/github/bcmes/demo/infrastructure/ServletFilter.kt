d2c2cb4b72d23765a07af3e4dd337026da1d2c34
brunosilva <brunomeloesilva@gmail.com>
2022-02-17 00:05:10 -0400

    Manipulando o Filter para: 2. Lendo o conteudo da request antes do controller, analisando que so pode ser lido uma vez

--- src/main/kotlin/com/github/bcmes/demo/infrastructure/ServletFilter.kt
+++ src/main/kotlin/com/github/bcmes/demo/infrastructure/ServletFilter.kt
@@ -3,6 +3,7 @@ package com.github.bcmes.demo.infrastructure
 import org.springframework.context.annotation.Configuration
 import org.springframework.util.AntPathMatcher
 import org.springframework.web.util.ContentCachingRequestWrapper
+import java.util.stream.Collectors
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 
@@ -30,10 +31,20 @@ class ServletFilter(
 
         //Analise 1:
         //Verifica se a rota chamada e uma das listadas acima
-        val request = req as HttpServletRequest
-        val bool = routes.any { route -> matcher.match(route, request.requestURI) }
-        println("===>$bool<===")
+        //val request = req as HttpServletRequest
+        //val bool = routes.any { route -> matcher.match(route, request.requestURI) }
+        //println("===>$bool<===")
         //Passa a request/response para a proxima entidade na cadeia
+        //chain?.doFilter(req, res)
+
+        //Analise 2:
+        //O getReader() so pode ser executado uma unica vez por request para ler o conteudo,
+        //se precisar ler o conteudo da request mais de uma vez, rode a rotina de cache para request da sessao.
+        //O erro disparado e java.lang.IllegalStateException
+        //No exemplo abaixo eu leio o conteudo e passo a requisicao para frente, quando ela for ser lida para inflar o
+        //objeto do controller, ela dara erro, pois ja foi lida.
+        val x = req?.reader?.lines()?.collect(Collectors.joining(System.lineSeparator()))
+        println(x)
         chain?.doFilter(req, res)
     }
 
